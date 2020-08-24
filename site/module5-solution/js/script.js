11b3b8d754f5184a0129c1e4c61b18fc4160cbfc
Ritesh Udhani <riteshudhani@gmail.com>
2020-08-24 14:28:01 -0500

    update with double quotes

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -106,7 +106,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-       var chosenCategoryShortName = randomCategory.short_name;
+       var chosenCategoryShortName = "'" + randomCategory.short_name + "'";
        var chosenCategoryname = "'" + randomCategory.name + "'";
 
 
